c9ada5d452150a015ae99e6fbc51fe98fac38d63
ghoshshreya <shrghosh08@gmail.com>
2023-02-22 12:20:48 +0530

    1 file modified, 1 added, 1 deleted

---
+++ selectionSort.js
@@ -0,0 +1,21 @@
+var selectionSort = function (arr) {
+  let arrLength = arr.length; // to avoid calculating everytime while looping
+  if (arrLength > 1) {
+    for (let x = 0; x < arrLength - 1; x++) {
+      // Find minimum index from unsorted section of the array
+      let minIndex = x;
+      for (let y = x + 1; y < arrLength; y++) {
+        if (arr[minIndex] > arr[y]) {
+          minIndex = y;
+        }
+      }
+      // Swap elements
+      let temp = arr[minIndex];
+      arr[minIndex] = arr[x];
+      arr[x] = temp;
+    }
+  }
+  return arr;
+};
+
+console.log(selectionSort([8, 0, 1, 7, 6]));
